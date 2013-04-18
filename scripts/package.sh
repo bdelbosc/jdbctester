1e1e96516230ecf397305ef2920f8236f197b28c
bdelbosc <bdelbosc@nuxeo.com>
2013-04-18 12:57:51 +0200

    cosmit

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,7 +2,7 @@
 HERE=$(cd $(dirname $0); pwd -P)
 cd $HERE/..
 mvn -o clean package || exit -1
-mvn -o dependency:copy-dependencies || exit -1
+mvn dependency:copy-dependencies || exit -1
 VERSION=`ls target/jdbctester*.jar | sed -e "s/^[^0-9]*//" | sed -e "s/.jar$//"`
 echo "### Packaging jdbctester $VERSION"
 DIST=./target/jdbctester
@@ -18,5 +18,5 @@ cp README.md $DIST
 chmod +x $DIST/jdbctester.sh || exit -1
 cd $DIST; cd ..
 tar czvf jdbctester-$VERSION.tgz jdbctester || exit -1
-echo "### Done: `readlink -e jdbctester-$VERSION.tgz`"
+echo "### Done: `pwd`/jdbctester-$VERSION.tgz"
 
